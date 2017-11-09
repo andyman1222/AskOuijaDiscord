f75ed44cdecdc5afd7227d181f981a2cde4ab197
Andy Herbert <andycherbert@gmail.com>
2017-11-08 23:42:10 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+// Load up the discord.js library
+const Discord = require("discord.js");
+
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
+const client = new Discord.Client();
+
+// Here we load the config.json file that contains our token and our prefix values. 
+const config = require("./config.json");
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+var users = new Array(), questions = new Array(), answers = new Array(), guilds = new Array(), askingQuestion = new Array();
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setGame('/r/AskOuija');
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  guilds.push();
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+
+});
+
+
+client.on("message", async message => {
+  var index;
+  if(guilds.indexOf(message.guild.id) == -1){
+    guilds.push(message.guild.id);
+    answers.push("");
+    questions.push("");
+    askingQuestion.push(false);
+    users.push(message.author.username);
+  }
+  index = guilds.indexOf(message.guild.id);
+  if(!(message.channel.name == "askouija")) return;
+  if(message.author.bot){
+    if(message.content.indexOf("\n\nThe answer is: ") !== -1) return;//message.pin();
+      return;
+  }
+  if(askingQuestion[index] == false){
+    if(message.content.indexOf(config.prefix) !== 0) return;
+    else if(message.content.toLowerCase() == "ouija, help"){
+      return message.channel.send("Find out more on reddit.com/r/AskOuija\nHOW TO DO IT: ask a question and have it answered by Ouija, or to help answer a question, send a 1 letter response or \"Goodbye\" to end the response.");
+    }
+    else{
+      questions[index] = message.content.substr(6, message.content.length);
+      askingQuestion[index] = true;
+      users[index] = message.author.username;
+      return message.channel.send("The question, asked by @" + users[index] + ", was: \n\n" + questions[index] + "");
+    }
+  }
+  else{
+    if(message.content.indexOf(config.prefix) !== -1){
+      if(message.content.substr(7, message.content.length).toLowerCase() == "question"){
+        return message.channel.send("The question, asked by @" + users[index] + ", was: \n\n" + questions[index] + "");
+      }
+    }
+    else if(message.content.toLowerCase().indexOf("goodbye") != -1){
+      askingQuestion[index] = false;
+      message.channel.send("The question, asked by @" + users[index] + ", was: \n\n" + questions[index] + "\n\nThe answer is: \n\n" + answers[index] + "");
+      questions[index] = "";
+      answers[index] = "";
+      users[index] = "";
+      return;
+    }
+    //else if(client.emojis.find(message.content.substring(1, message.length-1)) != undefined) answers[index] += client.emojis.find(message.content.substring(1, message.length-1)).toString();
+    else if(message.content.length > 1){
+      console.log("" + message.content.charCodeAt(0));
+      return message.delete();
+    } 
+    else answers[index] += message.content;
+  }
+});
+
+client.login(config.token);
+           
